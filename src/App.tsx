f651e9fdd30ddeba174ae3b84b5198e16b6931f2
sheharyarsaleem176177-cmd <sheharyarsaleem176177@gmail.com>
2025-12-10 00:37:58 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/creative-writing-hub-web/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
